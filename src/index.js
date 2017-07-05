1523adc7098339b47fbbf306ca9bdb502d9de0d9
Hiroki Ishiwatari <ishiwatari@fragmentof.net>
2017-07-06 06:38:31 +0900

    Reconstructing directories and update tasks

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { render } from 'react-dom'
 
-import '../stylesheets/style.scss'
-
 import configureStore from './store/configureStore'
 import App from './components/App'
 
